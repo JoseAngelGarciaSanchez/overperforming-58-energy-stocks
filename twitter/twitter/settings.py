85274906ee2860ff6e67decf5cd06a9e72429910
JAGS <78938133+Pse1234@users.noreply.github.com>
2023-01-22 12:31:07 +0100

    new spider

---
+++ twitter/twitter/settings.py
@@ -0,0 +1,28 @@
+# !!! # Crawl responsibly by identifying yourself (and your website/e-mail) on the user-agent
+USER_AGENT = 'mosefdatascience@gmail.com'
+
+# settings for spiders
+BOT_NAME = 'TweetScraper'
+LOG_LEVEL = 'INFO'
+
+SPIDER_MODULES = ['TweetScraper.spiders']
+NEWSPIDER_MODULE = 'TweetScraper.spiders'
+ITEM_PIPELINES = {
+    'TweetScraper.pipelines.SaveToFilePipeline':100,
+}
+
+# settings for where to save data on disk
+SAVE_TWEET_PATH = './Data/tweet/'
+SAVE_USER_PATH = './Data/user/'
+
+DOWNLOAD_DELAY = 1.0
+
+# settings for selenium
+from shutil import which
+SELENIUM_DRIVER_NAME = 'firefox'
+SELENIUM_BROWSER_EXECUTABLE_PATH = which('firefox')
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('geckodriver')
+SELENIUM_DRIVER_ARGUMENTS=['-headless']  # '--headless' if using chrome instead of firefox
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800
+}
