545836d850eebb3f5e571a59474662a26efc658d
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-01-26 23:10:18 +0100

    definitive version

--- webscrapping/twitter_scrapper.py
+++ webscrapping/twitter_scrapper.py
@@ -13,46 +13,53 @@ import urllib.parse
 
 class TwitterScrapper:
     """
-    Github:
-    @Pse1234, @sarrabenyahia
-    Automatic webscrapper of twitter media page. It enables you to get data from what you are interesed of.
-    In our case is for energy stocks. 
-    
+    LAST MODIFIED:
+        2023-01-26
+    AUTHORS GITHUB:
+        @Pse1234, @sarrabenyahia
+
+    A simple twitter webscrapper based on selenium 
+    It enables you to get data from what you are interesed of, for example: "TOTALENERGIES" 
+
     Parameters:
     -----------
-    research: list, default []
+    research: str, default []
+        words that you want to looking for in twitter
 
     link: str, default "www.twitter.com"
+        Webpage link for webscrapping, in our case twitter. 
+        We let this parameter on in case twitter decide to change it or you want a specific language, for example: "www.twitter.com/"
+
+    output_path: str, default "./../"
+        If you have the same architecture than us, you can change it to ./../data/ but creating a specific folder
     
-    output_path: str, default ""
-    
-    start_date: str, default 2017-10-31
-        date format yyyy-mm-dd 
+    start_date: str, default 2017-10-31 
+        date with format yyyy-mm-dd 
     
     end_date: str, default 2022-09-30
-        date format yyyy-mm-dd 
+        date with format yyyy-mm-dd 
     
     mail: str, default None
-        mail for accessing into twitter
+        mail of the user in case you want access into twitter
     
     username: str, default None
-        username for avoiding suspicious activity in private navigator
+        username of the user in case you want to access into twitter, as we are in private navigation, a suspicious alert may be activated
     
     password: str, default None
-        password for accessing into twitter
+        password of the user in case you want to access into twitter
 
     verbose: boolean default False
-        printing information about the algorithm
+        printing information about each step of the algorithm
 
     Note:
     -----
     Keep in mind that accessing to an account allows you to get extra data. The algorithm is constructed to avoid all the bot detections systems of twitter. We have test it for a thousand of times.
     If some of the xpath don't match anymore because twitter have changed their webpage, 
     you can adapt them: inspect the element of the webpage and copy the xpath.
-    Sometimes, you have to re-run the algorithm for great work.
-
+    Sometimes, you will have an error, you have to re-run the algorithm for great work.
     """
-    def __init__(self, research, link, output_path, start_date=None, end_date=None, mail=None, username=None, password=None, verbose: bool=False):
+
+    def __init__(self, research:str="Elon musk", link:str="https://twitter.com/", output_path:str="./../data/", start_date=None, end_date=None, mail=None, username=None, password=None, verbose: bool=False):
         self.research = research
         self.link = link
         self.output_path = output_path
@@ -108,7 +115,7 @@ class TwitterScrapper:
         return tweet
 
     def _open_set_up_chrome(self):
-        # create instance of web driver in incognito
+        """Opens and sets up chrome in incognito mode (private navigation), maximazing the windows for avoiding changes in xpath which are relative to the size of the windows due to proportionality CSS methods."""
         options = webdriver.ChromeOptions().add_argument("--incognito")
         driver = Chrome(options=options)
         driver.maximize_window()
@@ -120,7 +127,7 @@ class TwitterScrapper:
         return driver
 
     def _pop_out_notifications(self, driver):
-        """Popup Activate the notifications"""
+        """Deactivating the popup at the beggining of accessing to twitter (Activate the notifications popup)"""
         popup = driver.find_element(
             "xpath",
             '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/span/span',
@@ -130,7 +137,7 @@ class TwitterScrapper:
             print("good for pop out notifications")
 
     def _connexion(self, driver):
-        """Connexion avec un compte twitter"""
+        """Connexion with twitter accound using mail, username and password"""
         connexion = driver.find_element(
             "xpath",
             '//*[@id="layers"]/div/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/a/div/span/span',
@@ -155,6 +162,7 @@ class TwitterScrapper:
 
 
     def _suspicious_activity(self, driver):
+        """Deactivating the popup for suspicious activity due to incognito mode. For that, entering the username"""
         suspicious = driver.find_element(
             "xpath",
             '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input',
@@ -169,7 +177,7 @@ class TwitterScrapper:
 
 
     def _password(self, driver):
-        """On rentre le mot de passe"""
+        """Entering the password for finally accessing to our account"""
         password_input = driver.find_element(
             "xpath",
             '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input',
@@ -184,6 +192,7 @@ class TwitterScrapper:
 
 
     def _no_reinforcement(self, driver):
+        """Sometimes a popup for activating a reinforcement of the account security may appear. Then, it deactivates it."""
         no_reinforcement = driver.find_element(
             "xpath",
             '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[1]/div/div/svg',
@@ -195,25 +204,35 @@ class TwitterScrapper:
 
 
     def _advanced_link(self, driver):
+        """This enables us to search directly by the URL, avoiding many XPATH that could change, it allows you to specify some advanced parameters for the search"""
         company_name = self.research
         encoded_name = urllib.parse.quote(company_name)
-        advanced_link = f"https://twitter.com/search?q={encoded_name}%20until%3A{self.end_date}%20since%3A{self.start_date}&src=typed_query&f=live"
+        if self.end_date is not None and self.start_date is not None: 
+            advanced_link = f"https://twitter.com/search?q={encoded_name}%20until%3A{self.end_date}%20since%3A{self.start_date}&src=typed_query&f=live"
+        elif self.end_date is None and self.start_date is not None:
+            advanced_link = f"https://twitter.com/search?q={encoded_name}%20since%3A{self.start_date}&src=typed_query&f=live"
+        elif self.end_date is not None and self.start_date is None:
+            advanced_link = f"https://twitter.com/search?q={encoded_name}%20until%3A{self.end_date}&src=typed_query&f=live"
+        else:
+            advanced_link = f"https://twitter.com/search?q={encoded_name}&src=typed_query&f=live"
         driver.get(advanced_link)
         if self.verbose:
             print("good for advanced link")
         return driver
 
 
-    def _stock_pop_up_notification(self, driver):
+    def _last_pop_up_notification(self, driver):
+        """Sometimes an additionnal pop up could appear then it deactivates it"""
         popup = driver.find_element(
             "xpath",
-            '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div',
+            '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]',
         )
         popup.click()
         sleep(random.choice(self.list_seconds))
 
 
     def _get_recents(self, driver):
+        """get recents tweets from the 'lasts' page for recent activity"""
         recent = driver.find_element(
             "xpath",
             '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div[2]/nav/div/div[2]/div/div[2]/a/div/div/span',
@@ -222,7 +241,6 @@ class TwitterScrapper:
         if self.verbose:
             print("good for getting recents tweets")
         sleep(random.choice(self.list_seconds))
-
 
     def _collect_all_tweets_from_current_view(self, driver, lookback_limit=25):
         page_cards = driver.find_elements("xpath", '//article[@data-testid="tweet"]')
@@ -235,7 +253,7 @@ class TwitterScrapper:
 
     def _generate_tweet_id(self, tweet):
         if self.verbose:
-            print("good for getting recents tweets")
+            print("good for generating tweets id")
         return "".join(tweet)
 
     def _scroll_down_page(self, driver, last_position, scroll_attempt=0, max_attempts=3):
@@ -259,6 +277,7 @@ class TwitterScrapper:
         return last_position, end_of_scroll_region
 
     def _save_tweet_data_to_csv(self, records, output_path, mode="a+"):
+        """ Saving the data collected into the specified output path with csv format """
         header = [
             "User",
             "Handle",
@@ -280,7 +299,7 @@ class TwitterScrapper:
                 writer.writerow(records)
 
 
-    def launch(self) -> None:
+    def main(self) -> None:
 
         self._save_tweet_data_to_csv(None, self.output_path, "w")  # create file for saving records
         last_position = None
@@ -296,34 +315,43 @@ class TwitterScrapper:
             sleep(random.choice(self.list_seconds))
         except NoSuchElementException:
             print("No popup for activate notifications :)")
-        # Connexion
+        
+        if self.username is not None and self.mail is not None and self.password is not None:
+            # Connexion
+            sleep(random.choice(self.list_seconds))
+            self._connexion(driver)
+
+            # Ils considèrent qu'on a une activité suspecte :
+            try:
+                self._suspicious_activity(driver)
+            except NoSuchElementException:
+                print("No popup for suspicious activity :)")
+
+            # On rentre le mot de passe
+            self._password(driver)
+
+            # On ne veut pas renforcer la sécurité du compte :
+            try:
+                self._no_reinforcement(driver)
+            except NoSuchElementException:
+                print("No popup for reinforcing security :) ")
+
+            # Popup "Activate the notifications"
+            try:
+                sleep(random.choice(self.list_seconds))
+                self._pop_out_notifications(driver)
+            except NoSuchElementException:
+                sleep(random.choice(self.list_seconds))
+
+        # advanced research from entering the link with dates
+        self._advanced_link(driver)
         sleep(random.choice(self.list_seconds))
-        self._connexion(driver)
-
-        # Ils considèrent qu'on a une activité suspecte :
-        try:
-            self._suspicious_activity(driver)
-        except NoSuchElementException:
-            print("No popup for suspicious activity :)")
-
-        # On rentre le mot de passe
-        self._password(driver)
-
-        # On ne veut pas renforcer la sécurité du compte :
-        try:
-            self._no_reinforcement(driver)
-        except NoSuchElementException:
-            print("No popup for reinforcing security :) ")
 
         # Popup "Activate the notifications"
         try:
-            self._stock_pop_up_notification(driver)
+            self._pop_out_notifications(driver)
         except NoSuchElementException:
             sleep(random.choice(self.list_seconds))
-
-        # Recherche avancée pour avoir les bonne dates
-        self._advanced_link(driver)
-        sleep(random.choice(self.list_seconds))
 
         # Get the recents
         self._get_recents(driver)
@@ -340,26 +368,23 @@ class TwitterScrapper:
                 tweet_id = self._generate_tweet_id(tweet)
                 if tweet_id not in unique_tweets:
                     unique_tweets.add(tweet_id)
-                    self._save_tweet_data_to_csv(tweet, output_path)
+                    self._save_tweet_data_to_csv(tweet, self.output_path)
             last_position, end_of_scroll_region = self._scroll_down_page(driver, last_position)
         driver.quit()
 
-if __name__ == "__main__":
-    link = "https://twitter.com/"
+def launch():
+    # companies that we are interesed in for webscrapping
     already_done= "BP PLC, STORA ENSO OYJ-R SHS, INTERNATIONAL PAPER CO,UPM-KYMMENE OYJ, NEWMONT CORP,EXXON MOBIL CORP, VALERO ENERGY CORP, NUCOR CORP, BARRICK GOLD CORP, FMC CORP, FREEPORT-MCMORAN INC, TOTALENERGIES SE, CONOCOPHILLIPS, \
     ARCHER-DANIELS-MIDLAND CO, POSCO HOLDINGS INC -SPON ADR, BHP GROUP LTD-SPON ADR, TECK RESOURCES LTD-CLS B, RIO TINTO PLC-SPON ADR, WILMAR INTERNATIONAL LTD, MONDI PLC, ANGLO AMERICAN PLC, CENOVUS ENERGY INC, ALTAGAS LTD \
-    WESTLAKE CORP, GLENCORE PLC, MOSAIC CO/THE, MARATHON PETROLEUM CORP, PHILLIPS 66, WEYERHAEUSER CO, ENERGY TRANSFER LP,"
-    companies_string = "VIPER ENERGY PARTNERS LP,SUNOCO LP,WESTROCK CO,PEMBINA PIPELINE CORP,ALCOA CORP,ARCELORMITTAL,NUTRIEN LTD,NUTRIEN LTD,DOW INC,CORTEVA INC,OCCIDENTAL PETROLEUM CORP,ONEOK INC,CHEVRON CORP,PIONEER NATURAL RESOURCES CO,TARGA RESOURCES CORP,SCHLUMBERGER LTD,BAKER HUGHES CO,DEVON ENERGY CORP,HESS CORP,MARATHON OIL CORP,WILLIAMS COS INC,COTERRA ENERGY INC,APA CORP,EOG RESOURCES INC,KINDER MORGAN INC,EQT CORP,HALLIBURTON CO,DIAMONDBACK ENERGY INC"
+    WESTLAKE CORP, GLENCORE PLC, MOSAIC CO/THE, MARATHON PETROLEUM CORP, PHILLIPS 66, WEYERHAEUSER CO, ENERGY TRANSFER LP, VIPER ENERGY PARTNERS LP, SUNOCO LP,"
+    companies_string = "WESTROCK CO,PEMBINA PIPELINE CORP,ALCOA CORP,ARCELORMITTAL,NUTRIEN LTD,NUTRIEN LTD,DOW INC,CORTEVA INC,OCCIDENTAL PETROLEUM CORP,ONEOK INC,CHEVRON CORP,PIONEER NATURAL RESOURCES CO,TARGA RESOURCES CORP,SCHLUMBERGER LTD,BAKER HUGHES CO,DEVON ENERGY CORP,HESS CORP,MARATHON OIL CORP,WILLIAMS COS INC,COTERRA ENERGY INC,APA CORP,EOG RESOURCES INC,KINDER MORGAN INC,EQT CORP,HALLIBURTON CO,DIAMONDBACK ENERGY INC"
     companies_list = companies_string.split(",")
     for i in companies_list:
-        output_path = f"/Users/sarrabenyahia/Documents/GitHub/NLP_STOCKS/Data/webscraped_{'_'.join(i.split())}.csv"
+        output_path = f"./../data/webscraped_{'_'.join(i.split())}.csv"
         print(f"Scrapping: {i} in twitter")
-        TwitterScrapper(research=i, link=link, output_path=output_path, start_date="2017-10-31", end_date="2019-14-10", verbose=False, mail="mosefdatascience@gmail.com", username="mosefdata", password="MosefDataScience@").launch()
+        TwitterScrapper(research=i, output_path=output_path, start_date="2017-10-31", end_date="2022-09-30", mail="mosefdatascience@gmail.com", username="mosefdata", password="MosefDataScience@").main()
         print(f"{i} webscrapped")
     print("finished")
 
-# verbose avec des prints
-# definir le mdp et username -> if not normal research
-# travailler la liste de inputs au lieu de la boucle avec des inputs uniques et faire une option --update pour updater les fichiers qui ont déjà été webscrappés
-# start date: 2017-10-31
-# end date: 2022-09-30
+if __name__ == "__main__":
+    launch()
