f10e15bc72e6790657d16cb52f1106fcfc4e653c
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-01-27 10:57:31 +0100

    first clean preprocessing

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -10,11 +10,6 @@ import nltk
 nltk.download('punkt')
 nltk.download('perluniprops')
 
-
-"""TODO :
-Changer le df et faire un path relative et mettre la data dans son propre dossier  
-"""
-
 class PreprocessorPipeline:
     
     def __init__(self, path, output_path):
@@ -22,7 +17,7 @@ class PreprocessorPipeline:
         self.output_path = output_path
  
     def import_df(self, path):
-        print('Importing the csv')
+        print('---Importing the tweets...')
         spark = SparkSession.builder.appName("CSVtoTable").getOrCreate()
 
         df = (spark.read.format("csv")
@@ -36,13 +31,13 @@ class PreprocessorPipeline:
     def cast_columns(self, df):
         """ Cast good columns types 
         """
-        print('Changing the type of columns')
+        print('---Changing the type of columns...')
         df = df.select(*[col(c).cast("integer").alias(c) if c in ['ReplyCount','RetweetCount','LikeCount'] else c for c in df.columns])
         df = df.withColumn("PostDate", to_date(df["PostDate"]))
         return df
 
     def cleaning_tweets(self, df):
-        print('Importing the tweets')
+        print('---Cleaning the tweets with regex...')
         df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "[\n]+", " ")))
         df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "https?://[^ ]+", "")))
         df = df.withColumn("TweetText", trim(regexp_replace("TweetText", "@[^ ]+", "")))
@@ -51,21 +46,23 @@ class PreprocessorPipeline:
         df = df.withColumn("TweetText", regexp_extract("TweetText", "(2021|2017|2018|2019|2020|2022).*", 0))
         df = df.withColumn("TweetText", regexp_replace("TweetText", "[^a-zA-Z\\s]", ""))
 
-        df = df.na.drop(subset="TweetText") 
+        
         return df
 
     def dealing_with_na(self, df):
-        print('Dealing with na values')
+        print('---Dealing with na values...')
         df = df.na.fill(0, subset=['ReplyCount','RetweetCount','LikeCount'])
         df = df.dropna(subset=["TweetText"])
         return df
 
     def loosing_handle(self, df):
-        print('Loosing the @ for the handles')
+        print('---Loosing the @ for the handles...')
         df = df.withColumn("Handle", trim(regexp_replace("Handle", "@", "")))
         return df
 
+
     def filtering_english(self, df):
+        print('---Choosing only the english tweets...')
         df_pd = df.toPandas()
         def detect_en(text):  
             try:
@@ -75,18 +72,17 @@ class PreprocessorPipeline:
         df_pd = df_pd[df_pd['TweetText'].apply(detect_en)]
         spark = SparkSession.builder.appName("CSVtoTable").getOrCreate()
         df= spark.createDataFrame(df_pd) 
-
         return df
 
-    def creating_csv(self, df):
-        print('Creating the cleaned csv!')
+    def creating_csv(self, df, output_path):
+        print('---Creating the cleaned csv!')
         df.write.format("csv").mode("overwrite").option("header", "true").save(self.output_path)
         return df
 
 
 if __name__ == "__main__":
-    path = "./../data/webscraped_WEYERHAEUSER_CO1.csv"
-    output_path = "./../data/processed_df.csv"
+    path = "/Users/sarrabenyahia/Documents/GitHub/overperforming-58-energy-stocks/webscraped_WEYERHAEUSER_CO1.csv"
+    output_path = "/Users/sarrabenyahia/Documents/GitHub/overperforming-58-energy-stocks/processed_df.csv"
     preprocessing = PreprocessorPipeline(path =path, output_path=output_path)
     df = preprocessing.import_df(path)
     df = preprocessing.cast_columns(df)
@@ -94,7 +90,7 @@ if __name__ == "__main__":
     df = preprocessing.dealing_with_na(df)
     df = preprocessing.loosing_handle(df)
     df = preprocessing.filtering_english(df)
-    preprocessing.creating_csv(df)
+    preprocessing.creating_csv(df, output_path)
     print("Here is the result :) ")
     df.show()
-    #print(df.count())
+    
