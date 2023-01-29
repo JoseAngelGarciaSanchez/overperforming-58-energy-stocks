8af8eb8bfedd32cbd108d0b1eca014ff45a5a830
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-01-29 01:55:01 +0100

    push new bp

--- app/pages/B_Exploratory_analysis_on_tweets.py
+++ app/pages/B_Exploratory_analysis_on_tweets.py
@@ -10,7 +10,8 @@ import seaborn as sns
 DATE_COLUMN = "DATE"
 DATA_PATH = "./../data/stocks_data.xlsx"
 TWEETS_PATH = ["./../data_cleaned/webscraped_FMC_CORP.csv/part-00000-9fe4bf5d-f913-4d0c-a98a-8f1d088639ee-c000.csv",
-                "./../data_cleaned/webscraped_WEYERHAEUSER_CO.csv/part-00000-17a4305d-9672-4ddf-87fc-d180dcf01612-c000.csv"
+                "./../data_cleaned/webscraped_WEYERHAEUSER_CO.csv/part-00000-17a4305d-9672-4ddf-87fc-d180dcf01612-c000.csv",
+                "./../data_cleaned/webscraped_BP_PLC.csv/part-00000-8a74ec7d-2015-42d8-8960-ce8e11e5a3a2-c000.csv",                
 ]
 
 # Layout
@@ -322,4 +323,3 @@ elif len(options) == 3:
         def wordcloud_stock():
             st.pyplot(plt)
         wordcloud_stock()
-
